e72a69b030009ca40969ffc7cd886798a1197ef5
Michał Zatorski <michal_zatorski@o2.pl>
2024-02-29 20:07:26 +0100

    Add greetings

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -14,3 +14,4 @@ for store, product in grocery_list.items():
     print(f"Idę do {store.capitalize()} i kupuję tam {[k.capitalize() for k in product]}")
 
 print(f"W sumie kupuję {l} produktów")
+print("Pozdrowienia dla Mentora")
