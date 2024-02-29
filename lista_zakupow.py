6c2b566d2a9951336abc1b1c9edfce0581ff5b34
Michał Zatorski <michal_zatorski@o2.pl>
2024-02-29 18:56:20 +0100

    Add number of purchased items

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -4,6 +4,9 @@ grocery_list = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+l = 0
 for store, product in grocery_list.items():
+    l = l + len(product)
     print(f"Idę do {store.capitalize()} i kupuję tam {[k.capitalize() for k in product]}")
 
+print(f"W sumie kupuję {l} produktów")
